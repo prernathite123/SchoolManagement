494d1e6894d6a377bd0256c0628d723c80f86dd0
Aniket Arjun Khillare <aniketkhillare17@gmail.com>
2025-06-14 20:46:05 +0530

    added vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    host: "0.0.0.0",
+    port: 1704,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
-}) 
+      },
+    },
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Origin-Agent-Cluster": "?1",
+    },
+  },
+});
